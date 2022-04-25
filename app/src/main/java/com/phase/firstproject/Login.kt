5d175a0fe78433470a6982dbb5dd9ca2b2ccb994
moho23 <78mohammadhossein@gmail.com>
2022-04-25 12:51:54 +0430

    Add Login logic

--- app/src/main/java/com/phase/firstproject/Login.kt
+++ app/src/main/java/com/phase/firstproject/Login.kt
@@ -1,23 +1,30 @@
 package com.phase.firstproject
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import android.text.TextWatcher
-import android.util.Log
 import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
-import androidx.core.widget.doAfterTextChanged
 import com.phase.firstproject.databinding.ActivityLoginBinding
 import java.util.*
+import java.util.regex.Pattern
 
 class Login : AppCompatActivity() {
 
     private var _binding: ActivityLoginBinding? = null
     private val binding get() = _binding!!
 
-    fun isValidEmail(target: CharSequence?): Boolean {
+    private val validEmailAddressRegex =
+        Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE)
+
+    private fun isValid(emailStr: String): Boolean {
+        val matcher = validEmailAddressRegex.matcher(emailStr)
+        return matcher.find()
+    }
+
+    private fun isValidEmail(target: CharSequence?): Boolean {
         return !TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches()
     }
 
@@ -26,7 +33,7 @@ class Login : AppCompatActivity() {
 
         _binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        title = "MobileNetwork-Login";
+        title = "Login";
         var color = Random();
 
         binding.googleLogin.setOnClickListener {
@@ -51,8 +58,34 @@ class Login : AppCompatActivity() {
             )
         }
 
-        binding.loginEmailEditText.doAfterTextChanged {
+        binding.loginEmailEditText.addTextChangedListener { it ->
+            if (isValidEmail(it.toString())) {
+                binding.loginEmail.isErrorEnabled = false
+            } else {
+                binding.loginEmail.error = "Wrong email format";
+            }
+        }
 
+        binding.loginPasswordEditText.addTextChangedListener { it ->
+            if (binding.loginPassword.editText?.text?.length != 0) {
+                binding.loginPassword.isErrorEnabled = false
+            }
+        }
+
+        binding.loginFormButton.setOnClickListener {
+            if (binding.loginEmailEditText.length() != 0 && isValidEmail(binding.loginEmail.editText!!.text) && binding.loginPasswordEditText.length() != 0) {
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_LONG).show()
+            }
+            if (binding.loginEmailEditText.length() == 0 && binding.loginPasswordEditText.length() != 0) {
+                binding.loginEmail.error = "Enter a valid email to login"
+            }
+            if (binding.loginEmailEditText.length() != 0 && binding.loginPasswordEditText.length() == 0) {
+                binding.loginPassword.error = "Enter a password to login"
+            }
+            if (binding.loginEmailEditText.length() == 0 && binding.loginPasswordEditText.length() == 0) {
+                binding.loginPassword.error = "Enter a password"
+                binding.loginEmail.error = "Enter a valid email"
+            }
         }
     }
 }
